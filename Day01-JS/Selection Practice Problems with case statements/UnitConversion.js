3bca85244a4916ed83cbaf941d2fb390e2ff373a
Ankit_kumar <ankit95001kumar@gmail.com>
2025-03-01 10:44:18 +0530

    [Ankit] Sequence Practice Problem using case Done

---
+++ Day01-JS/Selection Practice Problems with case statements/UnitConversion.js
@@ -0,0 +1,10 @@
+let option = parseInt(process.argv[2]);
+let value = parseFloat(process.argv[3]);
+
+switch (option) {
+    case 1: console.log(value, "Feet =", value * 12, "Inches"); break;
+    case 2: console.log(value, "Feet =", value * 0.3048, "Meters"); break;
+    case 3: console.log(value, "Inches =", value / 12, "Feet"); break;
+    case 4: console.log(value, "Meters =", value / 0.3048, "Feet"); break;
+    default: console.log("Invalid Option");
+}
